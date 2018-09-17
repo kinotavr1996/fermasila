7997b4a3329210b8c143cafdd7108c04ccbe30fc
kinotavr1996 <olegolegovich1996@gmail.com>
2018-09-17 20:37:02 +0300

    Initial commit

---
+++ ferma.tv/templates/sila/html/com_content/article/procentPlan.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+
+// Create shortcuts to some parameters.
+$params  = $this->item->params;
+$images  = json_decode($this->item->images);
+$urls    = json_decode($this->item->urls);
+$canEdit = $params->get('access-edit');
+$user    = JFactory::getUser();
+$info    = $params->get('info_block_position', 0);
+JHtml::_('behavior.caption');
+?>
+
+<div class="item-page<?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Article">
+<div itemprop="articleBody">
+		<?php echo $this->item->text; ?>
+    </div>
+	</div>
+    
